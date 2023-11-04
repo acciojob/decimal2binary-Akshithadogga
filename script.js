0efce4fdfa33efb33f05db008cc1a9083fdbc3f2
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-04 20:04:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 function decimalToBinary(num) {
-  //Wr
-	  var str="";
-		  var ans;
-	      var div;
-	  if(num=0){
-		  return str+=0; 
+	//Write you code here
+	var dupnum=num;
+		  var str="";
+		  var newString = "";
+		  var rem;
+	      var extra;
+	  while(dupnum>1){
+		  rem=dupnum%2;//0,1,0
+		  extra=dupnum-rem;//10,4,2
+		  str=str.concat(rem);//0101
+		  dupnum=extra/2;//5,2,1
 	  }
-	  else if(num=1){
-		  return str+=1;
-	  }
-	  while(num>1){
-		  ans=num%2//0
-		  div=num-ans//2
-		  str+=ans;//0
-		  num=num/2;
-	  }
-	  return str;
+	
+	  if(dupnum==0){
+		 str=str.concat(0); 
+	}
+	else if(dupnum==1){
+		 str=str.concat(1);
+	}
+	 	
+		for (var i = str.length - 1; i >= 0; i--) {
+			newString += str[i];
+		}
+		return newString;	  
+		
+  }
+  // let strNum=prompt("enter number");
+  // let num=Number(strNum);
+  // let ans=decimalToBinary(num);
+  // console.log(ans);
+  window.decimalToBinary = decimalToBinary;
   
-}
-
-window.decimalToBinary = decimalToBinary;
