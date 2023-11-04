ea696e2c17e5316885e243ded1b278abe8a58f70
Akshithadogga <117561482+Akshithadogga@users.noreply.github.com>
2023-11-04 17:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 function decimalToBinary(num) {
-  //Write you code here
-		let str="";
-	if(num=0){
-		
-		return str+=0; 
-	}
-	else if(num=1){
-		return str+=1;
-	}
-	let ans;
-	let div;
-
-	while(num>1){
-		ans=num%2//1
-		div=num-ans//6
-		str+=ans;
-		num=num/2;
-	}
-	return str;
+  //Wr
+	  var str="";
+		  var ans;
+	      var div;
+	  if(num=0){
+		  return str+=0; 
+	  }
+	  else if(num=1){
+		  return str+=1;
+	  }
+	  while(num>1){
+		  ans=num%2//0
+		  div=num-ans//2
+		  str+=ans;//0
+		  num=num/2;
+	  }
+	  return str;
   
 }
 
